8912f5861ed0716b854da8aa2119fb4008a43fd3
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-02-26 19:56:18 -0300

    12

--- Mundo 2/Desafio054/Desafio054.py
+++ Mundo 2/Desafio054/Desafio054.py
@@ -1,4 +1,7 @@
-import random
-import datetime
-data = datetime.date.year
-print(data)
+
+
+for c in range(1, 8):
+    int(input('Em qual ano a {} nasceu? '.format(c)))
+ano = int(input('Qual o ano? '))
+if ano - c >= 18:
+    print()
