afd3e8dbfbfa5e9398543f3f7d48ac9eb18a77f1
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-02-28 20:28:20 -0300

    Create Aula014.py

---
+++ Mundo 2/Aula014/Aula014.py
@@ -0,0 +1,23 @@
+# c = 1
+# while c < 10:
+    # print(c)
+    # c = c + 1
+# print('fim')
+
+# n = 1
+# while n != 0:
+    # n = int(input('valor: '))
+# print('Fim')
+
+n = 1
+par = 0
+impar = 0 
+while n != 0:
+    n = int(input('valor: '))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+        impar += 1
+print('Você digitou {} pares e {} impares'.format(par, impar))
+print('Fim')
