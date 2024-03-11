1311cdc36d8819e352f5e4af4c88afb2c82f485b
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2024-03-11 07:38:52 -0300

    h

--- sorteio mega sena kk/meu.py
+++ sorteio mega sena kk/meu.py
@@ -1,4 +1,11 @@
 import random
 
-for i in range(6):
-    print(random.randint(1,60))
+a = []
+
+while len(a) < 6:
+    k = random.randint(1, 60)
+    if k not in a:
+        a.append(k)
+
+for i in range(len(a)):
+    print("No número {}, o valor é: {}".format(i+1, a[i]))
