cdc81925a126d55edfadcc81ebbfb6823ad37d56
Gabriel Santos / yKold <108306401+yKold@users.noreply.github.com>
2024-04-24 11:32:15 -0300

    s

---
+++ Programador Python/PythonDoZeroAoJogoDeRPG.py
@@ -0,0 +1,14 @@
+# lista = ["batata", "maca"]
+# for x in lista:
+#     print(x, end="") // ao final da linha ele continua
+
+# npcs = [
+#     {"name": "monstro 1", "level": 1},
+#     {"name": "monstro 2", "level": 2},
+#     {"name": "monstro 3", "level": 3},
+#     {"name": "monstro 4", "level": 4}
+# ]
+
+# for x in npcs:
+#     print(x["name"]) // procura o "name"
+
