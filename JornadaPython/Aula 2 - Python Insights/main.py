0580a2a0c095fe0e31e79ded6af4028f38c81bc1
Gabriel Santos / yKold <108306401+yKold@users.noreply.github.com>
2024-05-09 14:22:51 -0300

    Update main.py

--- JornadaPython/Aula 2 - Python Insights/main.py
+++ JornadaPython/Aula 2 - Python Insights/main.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import plotly.express as px
 
 tabela = pd.read_csv("JornadaPython\Aula 2 - Python Insights\cancelamentos_sample.csv")
 tabela = tabela.drop(columns="CustomerID")
 tabela = tabela.dropna()
-print(tabela.info())
+
+# print(tabela["cancelou"].value_counts(normalize=True))
+
+grafico = px.histogram(tabela, x="duracao_contrato")
+grafico.show()
