1c3de4c022acad65ae505ab93a08c5266475bf36
Gabriel Santos / yKold <108306401+yKold@users.noreply.github.com>
2024-07-02 13:33:04 -0300

    Update teste.py

--- UFV/Lista02/teste.py
+++ UFV/Lista02/teste.py
@@ -1,3 +1,20 @@
-x = 13
-if  12>x>7:
-    print("batata")
+num_guardados = []
+resultado = []
+range_max = 0
+
+for i in range(100):
+    x = int(input())
+    num_guardados.append(x)
+
+for i in num_guardados:
+        if i > range_max:
+            range_max = i
+            
+for i in range(range_max+1):
+    for j in num_guardados:
+        if i == j:
+            resultado.append(i)
+
+print(resultado)
+
+         
